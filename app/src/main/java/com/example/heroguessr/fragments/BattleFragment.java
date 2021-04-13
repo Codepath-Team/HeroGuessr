643fc783261aefde7a1987aaecd7472a5993ed1a
justin <justinbautista923@gmail.com>
2021-04-13 18:53:24 -0400

    battle fragment ui changes

--- app/src/main/java/com/example/heroguessr/fragments/BattleFragment.java
+++ app/src/main/java/com/example/heroguessr/fragments/BattleFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.heroguessr.R;
 
@@ -29,4 +30,6 @@ public class BattleFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+
 }
